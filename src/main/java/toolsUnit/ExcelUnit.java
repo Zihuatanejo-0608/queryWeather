4ca86f99b342e989a7a6f8bc058d8d187b009fa9
chukaimin <78079759@qq.com>
2020-10-19 10:30:33 +0800

    新增excel数据源读取方式

---
+++ src/main/java/toolsUnit/ExcelUnit.java
@@ -0,0 +1,77 @@
+package toolsUnit;
+
+import cn.hutool.core.io.FileUtil;
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by andy on 2020/10/15.
+ *
+ * jxl只支持xls格式的excel
+ *
+ */
+public class ExcelUnit {
+
+    private Workbook workbook;
+    private Sheet sheet;
+    private Cell cell;
+    int rows;
+    int columns;
+    private String excelName;
+    private String sheetNmae;
+    private ArrayList<String> arrkey = new ArrayList<>();
+
+    public ExcelUnit(String excelName, String sheetNmae){
+        super();
+        this.excelName = excelName;
+        this.sheetNmae = sheetNmae;
+    }
+
+    //获取excel表中的数据
+    public Object[][] getExcelData() throws BiffException,IOException{
+
+        workbook = Workbook.getWorkbook(new File(getPath()));
+        sheet = workbook.getSheet(sheetNmae);
+        rows = sheet.getRows();//行
+        columns = sheet.getColumns();//列
+        //返回值为Obj[][],定义一个多行单列的二维数组
+        HashMap<String,String>[][] arrmap = new HashMap[rows - 1][1];
+        //对数组中的元素haspmap初始化
+        if (rows > 1){
+            for (int i = 0; i < rows -1; i++){
+                arrmap[i][0] = new HashMap<>();
+            }
+        }else {
+            System.out.println("excel中没有数据!");
+        }
+        //获取列名,作为hashmao的key
+        for (int c = 0; c < columns; c++){
+            String cellvalue = sheet.getCell(c,0).getContents();
+            arrkey.add(cellvalue);
+        }
+        //遍历单元格,添加到hashmap中
+        for (int r =1; r < rows; r++){
+            for (int c = 0; c < columns; c++){
+                String cellvalue = sheet.getCell(c,r).getContents();
+                arrmap[r - 1][0].put(arrkey.get(c),cellvalue);
+            }
+        }
+        return arrmap;
+    }
+
+    //本地取文件相对路径
+    public String getPath(){
+        String absolutePath = FileUtil.getAbsolutePath(excelName);
+        System.out.println("数据资源路径:" + absolutePath);
+        return absolutePath;
+    }
+
+
+}
---
+++ src/test/java/queryServiceTest/QueryWeatherExcel.java
@@ -0,0 +1,39 @@
+package queryServiceTest;
+
+import cn.hutool.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import toolsUnit.ExcelUnit;
+import toolsUnit.HuToolHttpUnit;
+import toolsUnit.ToolsUnit;
+
+import java.util.Map;
+
+/**
+ * Created by andy on 2020/10/19.
+ */
+public class QueryWeatherExcel {
+
+    String urlHead = ToolsUnit.getPropertiesInfo("BaseUrl.properties", "weatherUrl");
+
+    @DataProvider(name = "cityCodeData")
+    public Object[][] testData() throws Exception{
+        ExcelUnit excelUnit = new ExcelUnit("queryServiceCase/queryWeather.xls","Sheet1");
+        return excelUnit.getExcelData();
+    }
+
+    @Test(description = "天气查询",dataProvider = "cityCodeData")
+    public void queryWeather(Map<String,String> map){
+
+        String cityCode = map.get("cityCode");
+        String url = urlHead + cityCode + ".html";
+        //执行请求
+        JSONObject response = HuToolHttpUnit.get(url, null, null);
+        //从返回json中获取需要的信息
+        JSONObject weatherinfo = response.getJSONObject("weatherinfo");
+        String cityName = weatherinfo.getStr("city");
+        //断言
+        Assert.assertEquals(cityName,map.get("expectCity"));
+    }
+}
